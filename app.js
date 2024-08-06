914c36779dc27ee3199792af2d5a570f6d964032
petritg <petrit.gashi@student.ehb.be>
2024-08-06 18:13:46 +0200

    testen of app runt op localhost

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const albumsRoutes = require('./routes/albums');
 
+const app = express();
+const port = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+    res.send('Server is running!');
+});
 
-
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
