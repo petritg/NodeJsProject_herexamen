ecf703264502078c588590c618b14c94cca3982b
petritg <petrit.gashi@student.ehb.be>
2024-08-13 20:50:10 +0200

    create

--- routes/bands_or_artists.js
+++ routes/bands_or_artists.js
@@ -3,7 +3,16 @@ const router = express.Router();
 const db = require('../config/db');
 
 // Create new Band/Artist
-
+router.post('/bands_or_artists', (req, res) => {
+    const { name, genre, location } = req.body;
+    const sql = 'INSERT INTO bands_or_artists (name, genre, location) VALUES (?, ?, ?)';
+    db.query(sql, [name, genre, location], (err, result) => {
+      if (err) {
+        return res.status(500).json({ error: err.message });
+      }
+      res.status(201).json({ message: 'Album created', albumId: result.insertId });
+    });
+  });
 // Read all Bands/Artists
 
 // Read a single Band/Artist by ID
